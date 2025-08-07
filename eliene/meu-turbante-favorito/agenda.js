73a454d66f615c49a88cca77722d554ac8ce1d84
Jaison <kammer.black@gmail.com>
2025-08-07 17:24:14 -0300

    atualizei data da finalização do projeto web.

--- eliene/meu-turbante-favorito/agenda.js
+++ eliene/meu-turbante-favorito/agenda.js
@@ -44,7 +44,7 @@ const tarefas = {
         { name: 'Revisões de diagramação', categoria: 'Revisão', dataInicio: '25/07/2025', dataFinal: '28/07/2025', started: false, check: false, used: false },
         { name: 'Criação do projeto web', categoria: 'Web', dataInicio: '24/07/2025', dataFinal: '24/07/2025', started: true, check: true, used: true },
         { name: 'Trabalhando no projeto web', categoria: 'Web', dataInicio: '24/07/2025', dataFinal: '31/07/2025', started: true, check: true, used: true },
-        { name: 'Finalizando projeto web', categoria: 'Web', dataInicio: '28/07/2025', dataFinal: '31/07/2025', started: true, check: false, used: true},
+        { name: 'Finalizando projeto web', categoria: 'Web', dataInicio: '28/07/2025', dataFinal: '08/08/2025', started: true, check: false, used: true},
         { name: 'Revisão de projeto web', categoria: 'Revisão', dataInicio: '31/07/2025', dataFinal: '08/08/2025', started: true, check: false, used: true },
         { name: 'Fechamento de diagramação (InDesign)', categoria: 'Fechamentos', dataInicio: '23/07/2025', dataFinal: '25/07/2025', started: false, check: false, used: false },
         { name: 'Fechamento de projeto web', categoria: 'Fechamentos', dataInicio: '31/07/2025', dataFinal: '09/08/2025', started: true, check: false, used: true },
